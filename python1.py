116f8ff0c801b827291ce29c4bc172f75062f342
Roberto Fuentealba <ro.fuentealba@duocuc.cl>
2022-03-09 16:05:11 -0300

    Corregido todo

--- python1.py
+++ python1.py
@@ -2,7 +2,7 @@
 #ejemplo
 x=0
 y=0
-y=int(input("Ingrese un dato para y"))
-x=int(input("Ingrese un dato para x"))
+y=int(input("Ingrese un dato para y: "))
+x=int(input("Ingrese un dato para x: "))
 res=x+y
-print(f"Tu resultado es",{res})
+print(f"Tu resultado es",res)
